d1b70b28f2c2f58d3b722c64ebead4cb73103010
koretechx <areeb.ahmed@koredigital.com.pk>
2024-09-27 17:18:17 +0500

    fix(pt-15): fix splash image syntex

--- src/app/components/SplashScreen.tsx
+++ src/app/components/SplashScreen.tsx
@@ -6,7 +6,7 @@ const splashScreen = () => {
   return (
     <div className="bg-slate-950 w-screen h-screen flex justify-center items-center">
       <Image
-        src="/assets/logogif.gif"
+        src="/assets/Logogif.gif"
         alt="Logo GIF"
         width={100} // Adjust the width as per your requirement
         height={100} // Adjust the height as per your requirement
