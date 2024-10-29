fe285ec052d785ac5a0b6f85b6209a9a44f8d402
koretechx <areeb.ahmed@koredigital.com.pk>
2024-10-29 20:58:43 +0500

    fix(pt-21):Fixes 3D Model Routing

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,15 +2,17 @@ interface RoundedSlideButtonProps {
   text: string;
   className?: string;
   disabled?: boolean;
+  onClick?:any;
 }
 
 const ButtonWrapper: React.FC<RoundedSlideButtonProps> = ({
   text,
   className,
+  onClick,
 }) => {
   return (
     <div className="flex min-h-[80px] 3xl:min-h-[70px] 4xl:min-h-[60px]   items-center justify-center">
-      <RoundedSlideButton text={text} className={className} />
+      <RoundedSlideButton text={text} className={className} onClick={onClick} />
     </div>
   );
 };
@@ -18,12 +20,13 @@ const ButtonWrapper: React.FC<RoundedSlideButtonProps> = ({
 const RoundedSlideButton: React.FC<RoundedSlideButtonProps> = ({
   text,
   className,
+  onClick
 }) => {
   return (
     <button
+    onClick={onClick}
       className={`
-        relative z-0 flex items-center gap-2 overflow-hidden rounded-lg border-[1px]  px-4 3xl:px-4 4xl:px-2 py-2 3xl:py-1 4xl:py-1 font-semibold
-        uppercase transition-all duration-500
+        relative z-0 flex items-center gap-2 overflow-hidden rounded-lg border-[1px]  px-4 3xl:px-4 4xl:px-2 py-2 3xl:py-1 4xl:py-1 font-semibold transition-all duration-500
         
         before:absolute before:inset-0
         before:-z-10 before:translate-x-[150%]
