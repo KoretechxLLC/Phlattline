898fc62391d4fdf51191aae653f123fed9b1d0a8
koretechx <areeb.ahmed@koredigital.com.pk>
2024-11-21 22:32:38 +0500

    feat(pt-92):create an API for fetching improved results

---
+++ src/app/api/getImprovementResult/route.ts
@@ -0,0 +1,156 @@
+import { prisma } from "@/app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { boolean } from "zod";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, message: "User ID is required" },
+        { status: 400 }
+      );
+    }
+    const userData = await prisma.users.findFirst({
+      where: {
+        id: Number(userId),
+        assessment_status: true,
+      },
+    });
+    if (!userData) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User have not attempted the assessments",
+        },
+        { status: 400 }
+      );
+    }
+
+    const assessments = await prisma.individual_assessments.findMany({
+      where: {
+        categoryId: 1,
+      },
+      include: {
+        individual_assessment_questions: {
+          include: { individual_assessment_options: true },
+        },
+        user_assessment_responses: {
+          where: { user_id: Number(userId) },
+        },
+      },
+    });
+
+    // Process the data to extract percentages
+    const results = await Promise.all(
+      assessments
+        .map(async (assessment) => {
+          const percentages: number[] = [];
+
+          // Process user responses
+          for (const response of assessment.user_assessment_responses) {
+            const question = assessment.individual_assessment_questions.find(
+              (q) => q.id === response.question_id
+            );
+
+            if (question) {
+              const matchingOption =
+                question.individual_assessment_options.find(
+                  (option) => option.option_text === response.selected_option
+                );
+
+              if (matchingOption) {
+                percentages.push(matchingOption.percentage);
+              }
+            }
+          }
+
+          // Calculate total threshold
+          const percentageThreshold =
+            percentages.reduce((prev, curr) => prev + curr, 0) /
+            (assessment.individual_assessment_questions.length || 1);
+
+          let coursesData: any;
+
+          if (percentageThreshold > 0 && percentageThreshold < 90) {
+            // Fetch relevant courses
+            const courses = await prisma.user_courses.findMany({
+              where: {
+                user_id: Number(userId),
+                status: "completed",
+                courses: {
+                  course_for: assessment.id,
+                },
+              },
+              include: {
+                courses: {
+                  select: {
+                    assessments: {
+                      select: {
+                        questions: true,
+                      },
+                    },
+                  },
+                },
+              },
+            });
+
+            // Process course data
+            coursesData = await Promise.all(
+              courses.map(async (course) => {
+                const courseResponses =
+                  await prisma.coursesAssessmentResponse.findMany({
+                    where: {
+                      userId: Number(userId),
+                      courseId: Number(course.course_id),
+                    },
+                  });
+
+                const questions =
+                  course.courses.assessments?.[0]?.questions || [];
+
+                const finalData = questions
+                  .map((question) => {
+                    const response = courseResponses.find(
+                      (resp) => resp.questionId === question.id
+                    );
+
+                    return response?.answer === question.correct_answer
+                      ? 100
+                      : 0;
+                  })
+                  .filter(Boolean);
+
+                const totalPercentages = finalData.reduce(
+                  (prev: any, curr: any) => prev + curr,
+                  0
+                );
+
+                return {
+                  courseId: course.course_id,
+                  totalPercentages: totalPercentages / questions.length,
+                  assessmentId: assessment.id,
+                  title: assessment.title,
+                };
+              })
+            );
+          }
+
+          if (coursesData) {
+            return coursesData;
+          }
+        })
+        .filter(Boolean)
+    );
+
+    let data = results.filter(Boolean);
+
+    return NextResponse.json({ success: true, data: data }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { success: false, data: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/components/InitialAssessmentsReport.tsx
@@ -0,0 +1,292 @@
+"use client";
+
+import { useConfig } from "@/app/hooks/use-config";
+import {
+  fetchAssessments,
+  resetSuccess,
+} from "@/redux/slices/individualassessment.slice";
+import { fetchAssessmentsResponse } from "@/redux/slices/individualAssessmentResponse.slice";
+import { RootState } from "@/redux/store";
+import dynamic from "next/dynamic";
+
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import GraphLoader from "./graphLoader";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+interface AssessmentTrackerProps {
+  height?: number;
+  chartType?: "bar" | "area";
+  categories?: string[];
+}
+
+const generateColors = (numFields: number) => {
+  const colors = [];
+  for (let i = 0; i < numFields; i++) {
+    colors.push(i % 2 === 0 ? "#FF0700" : "#FDF53F");
+  }
+  return colors;
+};
+
+const calculatePercentages = (assessmentsResponse: any, assessments: any) => {
+  let calculatedGraph =
+    assessments &&
+    assessments?.length > 0 &&
+    assessments
+      .map((index: any) => {
+        let responseData = assessmentsResponse
+          ?.map((e: any) => {
+            if (e?.question?.individual_assessment_id == index?.id) {
+              let assessementTrack = e?.question?.individual_assessment_options
+                .map((option: any) => {
+                  if (e?.selected_option == option?.option_text) {
+                    return option.percentage;
+                  }
+                })
+                .filter(Boolean);
+
+              return {
+                id: e?.question?.individual_assessment_id,
+                percentage: assessementTrack,
+              };
+            }
+          })
+          .filter(Boolean);
+
+        let totalPercentage = 0;
+
+        responseData.forEach((item: any) => {
+          const currentSum =
+            item?.percentage.length > 0
+              ? item?.percentage.reduce((sum: any, val: any) => sum + val, 0)
+              : 0;
+
+          totalPercentage += currentSum;
+        });
+
+        if (responseData && responseData.length > 0) {
+          return {
+            title: index.title,
+            percentage:
+              totalPercentage / index.individual_assessment_questions.length,
+          };
+        }
+      })
+      .filter(Boolean);
+
+  return calculatedGraph;
+};
+
+const InitialAssessmentsReport = ({
+  height = 170,
+  chartType = "bar",
+}: AssessmentTrackerProps) => {
+  const [config] = useConfig();
+  const { isRtl } = config;
+
+  const [allCategories, setAllCategories] = useState<any>([]);
+  const { assessments, loading, error, success } = useSelector(
+    (state: RootState) => state.assessment
+  );
+
+  const {
+    responseLoading,
+    responseError,
+    assessmentsResponse,
+    responseSuccess,
+  } = useSelector((state: RootState) => state.assessmentResponse);
+
+  const { userData } = useSelector((state: RootState) => state.auth);
+  const userId: any = userData?.id;
+  const dispatch: any = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      fetchAssessments({
+        filter: {
+          page: 1,
+          size: 5,
+          categoryId: 1,
+        },
+        type: "general",
+      })
+    );
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchAssessmentsResponse({ userId }));
+  }, [dispatch, userId]);
+
+  useEffect(() => {
+    if (responseSuccess) {
+      dispatch(resetSuccess());
+    }
+  }, [responseSuccess]);
+  useEffect(() => {
+    if (assessments && assessments.length > 0) {
+      const categories =
+        assessments?.length > 0 ? assessments?.map((e: any) => e?.title) : [];
+      setAllCategories(categories);
+    }
+  }, [assessments]);
+
+  const data = calculatePercentages(assessmentsResponse, assessments);
+
+  const chartColors = generateColors(allCategories.length);
+  const series = [
+    {
+      name: "Assessments",
+      data:
+        data &&
+        data.length > 0 &&
+        data.map((value: any, index: any) => ({
+          x: value?.title,
+          y: value?.percentage,
+          fillColor: chartColors[index],
+        })),
+    },
+  ];
+
+  const options: any = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        endingShape: "rounded",
+        columnWidth: "5%",
+      },
+    },
+    legend: {
+      show: true,
+      position: "top",
+      horizontalAlign: "right",
+      fontSize: "20px",
+      offsetY: -60,
+      markers: {
+        width: 8,
+        height: 8,
+        offsetY: -1,
+        offsetX: -5,
+        radius: 12,
+      },
+      labels: {
+        colors: "#62626280",
+      },
+      itemMargin: {
+        horizontal: 12,
+        vertical: 0,
+      },
+    },
+    title: {
+      align: "left",
+      offsetY: 13,
+      offsetX: isRtl ? "0%" : 0,
+      floating: false,
+      style: {
+        fontSize: "20px",
+        fontWeight: "500",
+        color: "#62626280",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"],
+    },
+    yaxis: {
+      percentage: [0, 10, 30, 70, 100],
+      labels: {
+        style: {
+          colors: "#ffffff",
+        },
+      },
+    },
+    xaxis: {
+      categories: allCategories,
+      labels: {
+        style: {
+          colors: "#ffffff",
+        },
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    tooltip: {
+      theme: "dark",
+      style: {},
+      y: {
+        formatter: function (val: number) {
+          return val + "%";
+        },
+      },
+    },
+    grid: {
+      show: false,
+      borderColor: "#62626280",
+      strokeDashArray: 10,
+      position: "back",
+    },
+    responsive: [
+      {
+        breakpoint: 600,
+        options: {
+          legend: {
+            position: "bottom",
+            offsetY: 8,
+            horizontalAlign: "center",
+          },
+          plotOptions: {
+            bar: {
+              columnWidth: "30%",
+            },
+            chart: {
+              width: "50%",
+            },
+          },
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      {responseLoading ? (
+        <div className="text-center text-gray-300">
+          <GraphLoader />
+        </div>
+      ) : data && data.length > 0 ? (
+        <div className="w-full max-h-[350px] sm:h-[150px] md:h-[180px] lg:h-[250px] ">
+          <Chart
+            options={options}
+            series={series}
+            type={chartType}
+            height="100%"
+            width="100%"
+          />
+        </div>
+      ) : (
+        <div className="text-center text-gray-300 py-20">
+          Please Submit Assessments First!
+        </div>
+      )}
+    </>
+  );
+};
+
+export default InitialAssessmentsReport;
