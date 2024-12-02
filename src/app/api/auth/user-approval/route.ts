65c86bc5afc51884be5d740470ea727889d468eb
M.Abdullah <m.abdullah@koredigital.com.pk>
2024-12-02 22:24:19 +0500

    feat(pt-118):integrate users or employee and data

--- src/app/api/auth/user-approval/route.ts
+++ src/app/api/auth/user-approval/route.ts
@@ -4,49 +4,82 @@ import { sendEmail } from "@/helper/send-email";
 
 export async function PUT(request: NextRequest) {
   try {
-    const { userId, status } = await request.json();
+    const { userId, status, employee_id } = await request.json();
 
-    if (!userId || !status) {
+    if ((!userId && !employee_id) || !status) {
       return NextResponse.json(
-        { message: "required fields are missing.", success: false },
+        { message: "Required fields are missing.", success: false },
         { status: 400 }
       );
     }
 
     if (status !== "approved" && status !== "rejected") {
       return NextResponse.json(
-        { message: "invalid status provided.", success: false },
+        { message: "Invalid status provided.", success: false },
         { status: 400 }
       );
     }
 
-    const user = await prisma.users.findUnique({
-      where: { id: userId },
-    });
+    let updatedUser;
+    let user;
 
-    if (!user) {
-      return NextResponse.json(
-        { message: "User not found.", success: false },
-        { status: 404 }
-      );
+    if (userId) {
+      user = await prisma.users.findUnique({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        return NextResponse.json(
+          { message: "User not found.", success: false },
+          { status: 404 }
+        );
+      }
+
+      if (user.status !== "pending") {
+        return NextResponse.json(
+          { message: "User status is not pending.", success: false },
+          { status: 400 }
+        );
+      }
+
+      updatedUser = await prisma.users.update({
+        where: { id: userId },
+        data: { status: status },
+      });
+    } else if (employee_id) {
+      user = await prisma.users.findFirst({
+        where: { employee_id: employee_id },
+      });
+
+      if (!user) {
+        return NextResponse.json(
+          {
+            message: "User not found with the given employee_id.",
+            success: false,
+          },
+          { status: 404 }
+        );
+      }
+
+      if (user.status !== "pending") {
+        return NextResponse.json(
+          { message: "User status is not pending.", success: false },
+          { status: 400 }
+        );
+      }
+
+      // Update users model
+      updatedUser = await prisma.users.update({
+        where: { id: user.id },
+        data: { status: status },
+      });
+
+      // Update employees model
+      await prisma.employees.update({
+        where: { id: employee_id },
+        data: { status: status },
+      });
     }
-
-    if (user.status !== "pending") {
-      return NextResponse.json(
-        { message: "User status is not pending.", success: false },
-        { status: 400 }
-      );
-    }
-
-    // const updatedUser = await prisma.users.update({
-    //   where: { id: userId },
-    //   data: { status: user.status == "approved"? "pending":"approved"},
-    // });
-
-    const updatedUser = await prisma.users.update({
-      where: { id: userId },
-      data: { status: status },
-    });
 
     // Send approval email
     const subject = "Your Account Has Been Approved";
@@ -54,7 +87,7 @@ export async function PUT(request: NextRequest) {
       <b>Congratulations!</b><p>Your account has been approved. You can now log in.</p>
     `;
 
-    await sendEmail({ email: user.email, subject, message });
+    await sendEmail({ email: user?.email, subject, message });
 
     return NextResponse.json({
       message: "User approved successfully and notification email sent.",
